f03af65ac835336cb3a92912fb7d3763da960799
Sophia <sophiaduan1997@gmail.com>
2016-09-30 18:45:14 -0400

    Add a change

--- calc.py
+++ calc.py
@@ -18,8 +18,8 @@ def mod(a, b):
 def main():
 	a = 66	#First number
 	b = 33	#Second number
-	result = mod (a,b)	# First divided by second
-	print("The result is %.2f" % result)
+	result = div (a,b)	# First divided by second
+	print("The result is %.2f" ,result)
 
 if __name__ == "__main__":
 	main()
